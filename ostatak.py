c6774091b046cf828431970a1148304110593732
Marinalatkovic <124623550+Marinalatkovic@users.noreply.github.com>
2023-02-06 20:13:19 +0100

    Add files via upload

---
+++ ostatak.py
@@ -0,0 +1,14 @@
+x = 523134
+y = 325423
+z = x % y
+print(z)
+if (z == 0 ) :
+    print("Ostatak ne postoji")
+else :
+   
+    print("Ostatak postoji")
+if (z > 1000 ) :
+        print("Ostatak je > 1000 ") 
+else :
+            
+ print("Ostatak je < 1000")
